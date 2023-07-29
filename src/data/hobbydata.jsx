9e0fbe300ddf6a345a49e67cb33cc960d2fefb61
mirosteiger <52956540+mirosteiger@users.noreply.github.com>
2023-07-29 19:46:20 +0200

    urls as string array

--- src/data/hobbydata.jsx
+++ src/data/hobbydata.jsx
@@ -1,17 +1,24 @@
 import { turntable } from '../assets/images/3d/turntable';
+import hammer from '../assets/images/3d/mjölnir.png';
 import wheeler from '../assets/videos/5wheeler.mp4';
 
 export const hobbyData = [
 	{
 		type: 'image',
-		title: 'hammer',
+		title: 'Turntable',
 		imgs: turntable,
+		tags: ['3d'],
+	},
+	{
+		type: 'image',
+		title: 'Hammer',
+		imgs: [hammer],
 		tags: ['3d'],
 	},
 	{
 		type: 'video',
 		title: '5-Wheeler',
-		imgs: wheeler,
+		imgs: [wheeler],
 		tags: ['3d'],
 	},
 ];
