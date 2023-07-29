260e8bf392a0bcefc6772b5e41061b06778981e4
mirosteiger <52956540+mirosteiger@users.noreply.github.com>
2023-07-29 21:01:39 +0200

    added media

---
+++ src/assets/images/3d/knoopit/index.js
@@ -0,0 +1,11 @@
+import k1 from './cc1.png';
+import k2 from './color_map.png';
+import k3 from './displace.png';
+import k4 from './metalness_color.png';
+import k5 from './render_placeholder.jpg';
+import k6 from './roughness0-3.png';
+import k7 from './transmission_20.png';
+
+const knoop = [k1, k2, k3, k4, k5, k6, k7];
+
+export { knoop };
--- src/data/hobbydata.jsx
+++ src/data/hobbydata.jsx
@@ -1,6 +1,8 @@
 import { turntable } from '../assets/images/3d/turntable';
 import hammer from '../assets/images/3d/mjölnir.png';
 import wheeler from '../assets/videos/5wheeler.mp4';
+import { ufo } from '../assets/images/3d/ufo';
+import { knoop } from '../assets/images/3d/knoopit';
 
 export const hobbyData = [
 	{
@@ -16,9 +18,21 @@ export const hobbyData = [
 		tags: ['3d'],
 	},
 	{
+		type: 'image',
+		title: 'Ufo',
+		imgs: ufo,
+		tags: ['3d'],
+	},
+	{
 		type: 'video',
-		title: '5-Wheeler',
+		title: '5-Wheeler ',
 		imgs: [wheeler],
+		tags: ['3d'],
+	},
+	{
+		type: 'image',
+		title: 'Knoop.it',
+		imgs: knoop,
 		tags: ['3d'],
 	},
 ];
